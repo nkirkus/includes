c59ed7e72d73401b9be587f57b279693a8555c6a
nkirkus <noah.kirkus@pharmmd.com>
2013-09-26 21:28:08 +0000

    Changed test.sh contents a bit.

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ source Functions.sh
 
 emails="noah.kirkus@pharmmd.com"
 
-SendMail "$emails" "PMD Utility <noah.kirkus@pharmmd.com>" "Test Email" "Just a test..."
+SendMail "$emails" "PMD Utility <noah.kirkus@pharmmd.com>" "Test-Email" "Just a test..."
