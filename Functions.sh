76a8e080778a19da6a56f1fd30b67bd8052ff081
nkirkus <noah.kirkus@pharmmd.com>
2013-09-26 20:20:27 +0000

    Added example to Function.sh - SendEmail

--- Functions.sh
+++ Functions.sh
@@ -2,6 +2,8 @@
 
 
 # SendMail $1=TO $2=FROM $3=SUBJECT $4=BODY
+# example: SendMail "email@address.com" "DisplayName <email@address.com>" "Example Subject" "`cat message.tmp`"
+
 SendMail()
 {
         [ -z $1 ] && echo "Error: SendMail - No arguments supplied." && exit 1;
