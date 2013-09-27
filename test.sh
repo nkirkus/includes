b1b3645cea4d5d8e9de33919cf7c437c1854971e
nkirkus <noah.kirkus@pharmmd.com>
2013-09-27 15:55:37 +0000

    Adjusted email addresses

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 source Functions.sh
 
-emails="noah.kirkus@pharmmd.com"
+emails="noah.kirkus@pharmmd.com,noah.kirkus@pharmmd.com"
 
 SendMail "$emails" "PMD Utility <noah.kirkus@pharmmd.com>" "Test-Email" "Just a test..."
